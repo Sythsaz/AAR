f8922029e72691d13429d3d2a6ef43a74b94f3bc
Sythsaz <ashtonparrott@gmail.com>
2024-03-17 15:46:40 -0600

    started login form

---
+++ AAR/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AAR
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool _isLoginFormVisible = true;
+        private bool _isTripInfoVisible = false;
+
+        public bool IsLoginFormVisible
+        {
+            get { return _isLoginFormVisible; }
+            set { _isLoginFormVisible = value; OnPropertyChanged(); }
+        }
+
+        public bool IsTripInfoVisible
+        {
+            get { return _isTripInfoVisible; }
+            set { _isTripInfoVisible = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- AAR/MainWindow.xaml.cs
+++ AAR/MainWindow.xaml.cs
@@ -1,29 +1,21 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AAR
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel(); // Set ViewModel as DataContext
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-
+            // Add your login logic here
+            MessageBox.Show("Login button clicked!");
         }
     }
 }
